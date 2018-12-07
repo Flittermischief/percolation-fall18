01ceaee5bb59cd203fec5b2a8e9baa522ae8ae8e
Flittermischief <kmc113@duke.edu>
2018-12-07 02:23:42 -0500

    B001b 1

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -13,28 +13,35 @@ public class PercolationBFS extends PercolationDFSFast {
 		Queue<Integer> myQ = new LinkedList<>(); 
 		myGrid[row][col] = FULL;
 		myQ.add(row*myGrid.length+col);
-		int dQd = myQ.remove();
-		
-		int dQdCol = dQd%myGrid.length;
-		int dQdRow = (dQd-dQdCol)/myGrid.length;
-		
-		if (inBounds(dQdRow - 1, dQdCol) && isOpen(dQdRow - 1, dQdCol) && (!isFull(dQdRow - 1, dQdCol))) {
-			myGrid[dQdRow - 1][dQdCol] = FULL;
-			myQ.add(myGrid[dQdRow - 1][dQdCol]);
-		}
-		if (inBounds(dQdRow + 1, dQdCol) && isOpen(dQdRow + 1, dQdCol) && (!isFull(dQdRow + 1, dQdCol))) {
-			myGrid[dQdRow + 1][dQdCol] = FULL;
-			myQ.add(myGrid[dQdRow + 1][dQdCol]);
-		}
-		if (inBounds(dQdRow, dQdCol - 1) && isOpen(dQdRow, dQdCol - 1) && (!isFull(dQdRow, dQdCol - 1))) {
-			myGrid[dQdRow][dQdCol - 1] = FULL;
-			myQ.add(myGrid[dQdRow][dQdCol - 1]);
-		}
-		if (inBounds(dQdRow, dQdCol + 1) && isOpen(dQdRow, dQdCol + 1) && (!isFull(dQdRow, dQdCol + 1))) {
-			myGrid[dQdRow][dQdCol + 1] = FULL;
-			myQ.add(myGrid[dQdRow][dQdCol + 1]);
-		}
-		
-	}
+		int a=0;
+		while (myQ.size() != 0 || a==0){
+			a=0;
+			int dQd = myQ.remove();
 
+			int dQdCol = dQd%myGrid.length;
+			int dQdRow = dQd/myGrid.length; //(dQd-dQdCol)/myGrid.length;
+
+			if (inBounds(dQdRow - 1, dQdCol) && isOpen(dQdRow - 1, dQdCol) && (!isFull(dQdRow - 1, dQdCol))) {
+				myGrid[dQdRow - 1][dQdCol] = FULL;
+				myQ.add(myGrid[dQdRow - 1][dQdCol]);
+				dfs(dQdRow - 1, dQdCol); 	// B001b
+			}
+			if (inBounds(dQdRow + 1, dQdCol) && isOpen(dQdRow + 1, dQdCol) && (!isFull(dQdRow + 1, dQdCol))) {
+				myGrid[dQdRow + 1][dQdCol] = FULL;
+				myQ.add(myGrid[dQdRow + 1][dQdCol]);
+				dfs(dQdRow + 1, dQdCol); 	// B001b
+			}
+			if (inBounds(dQdRow, dQdCol - 1) && isOpen(dQdRow, dQdCol - 1) && (!isFull(dQdRow, dQdCol - 1))) {
+				myGrid[dQdRow][dQdCol - 1] = FULL;
+				myQ.add(myGrid[dQdRow][dQdCol - 1]);
+				dfs(dQdRow, dQdCol - 1); 	// B001b
+			}
+			if (inBounds(dQdRow, dQdCol + 1) && isOpen(dQdRow, dQdCol + 1) && (!isFull(dQdRow, dQdCol + 1))) {
+				myGrid[dQdRow][dQdCol + 1] = FULL;
+				myQ.add(myGrid[dQdRow][dQdCol + 1]);
+				dfs(dQdRow, dQdCol + 1); 	// B001b
+			}
+			a=1;
+		}
+	}
 }
